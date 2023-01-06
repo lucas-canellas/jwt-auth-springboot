756326b8ac72b5963ae6ef1b0a5e3d693c3b6522
Lucas David <lucascanellasdev@gmail.com>
2023-01-05 21:27:51 -0300

    ...

---
+++ src/main/java/com/lucasdc/security/auth/AuthenticationService.java
@@ -0,0 +1,49 @@
+package com.lucasdc.security.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.lucasdc.security.config.JwtService;
+import com.lucasdc.security.user.Role;
+import com.lucasdc.security.user.User;
+import com.lucasdc.security.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+            .firstname(request.getFirstname())
+            .lastname(request.getLastname())
+            .email(request.getEmail())
+            .password(passwordEncoder.encode(request.getPassword()))
+            .role(Role.USER)
+            .build();
+        
+            repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authenticate(RegisterRequest request) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(), 
+                request.getPassword()
+            )
+        );
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }    
+}
---
+++ src/main/java/com/lucasdc/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.lucasdc.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
